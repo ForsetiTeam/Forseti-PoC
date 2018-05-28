be93ebe56560b6f76e983e01f5d1769452b8f95f
zeleniy <zeleniy00@yandex.ru>
2018-05-28 13:51:52 +0700

    server validation

--- src/server/models/VoteModel.ts
+++ src/server/models/VoteModel.ts
@@ -1,16 +1,22 @@
 import {prop, Ref, Typegoose} from "typegoose";
 import { User } from "./UserModel";
 
+export enum Decision {
+  APPROVE = 'approve',
+  DISAPPROVE = 'disapprove',
+  ABSTAIN = 'abstain'
+}
+
 export class Vote extends Typegoose {
 
   @prop({ required: true })
   public user: Ref<User>;
 
-  @prop({ })
-  public decision: string;
+  @prop({ enum: Decision, required: true })
+  public decision: Decision;
 
   @prop({ })
-  public sign: string;
+  public sig: string;
 }
 
 const VoteModel = new Vote().getModelForClass(Vote);
---
+++ src/server/validators/disputeSchemas.ts
@@ -0,0 +1,81 @@
+import { sigLength, addressLength } from './ethConsts';
+import { Decision } from '../models/VoteModel'
+
+export function validateGetDisputeList(req) {
+  return {
+    author: {
+      optional: {options: [{checkFalsy: true}]},
+      isBoolean: {
+        errorMessage: 'Accepted values: "true" and "false".'
+      },
+    },
+    arbiter: {
+      optional: {options: [{checkFalsy: true}]},
+      isBoolean: {
+        errorMessage: 'Accepted values: "true" and "false".'
+      },
+    },
+    answered: {
+      optional: {options: [{checkFalsy: true}]},
+      isBoolean: {
+        errorMessage: 'Accepted values: "true" and "false".'
+      },
+    },
+  };
+}
+
+export function validateCreateDispute(req) {
+  return {
+    title: {
+      isLength: {
+        options: [3, 255],
+        errorMessage: 'Invalid length'
+      },
+    },
+    description: {
+      isLength: {
+        options: [3, 2047],
+        errorMessage: 'Invalid length'
+      },
+    },
+    community: {
+      isLength: {
+        options: addressLength,
+        errorMessage: 'Invalid length'
+      },
+    },
+    arbitersNeed: {
+      isInt: {
+        options: {min: 1},
+        errorMessage: 'Min value: 1'
+      }
+    }
+  }
+}
+
+export function validateVoteDispute(req) {
+  return {
+    decision: {
+      isIn: {
+        options: {list: Object.values(Decision)},
+        errorMessage: 'Invalid value'
+      },
+    },
+    sig: {
+      isLength: {
+        options: sigLength,
+        errorMessage: 'Invalid length'
+      },
+    },
+  }
+}
+
+export function validateStartDispute(req) {
+  return {
+    ethAddress: {
+      isLength: {
+        options: addressLength
+      },
+    },
+  }
+}
