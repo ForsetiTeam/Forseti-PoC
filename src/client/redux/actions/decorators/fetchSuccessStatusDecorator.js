edd7962a77bbb38b1ff14a59fa530c836751cc4e
zeleniy <zeleniy00@yandex.ru>
2018-06-04 11:20:27 +0700

    fix hide error messages in redux decorator

--- src/client/redux/actions/decorators/fetchSuccessStatusDecorator.js
+++ src/client/redux/actions/decorators/fetchSuccessStatusDecorator.js
@@ -2,6 +2,7 @@ export default function (resp) {
   return resp
     .then(response => {
       if (response.status !== 200 && response.status !== 304) {
+        if (response.data && response.data.message) return Promise.reject(response.data.message);
         return (response.statusText !== 'Bad Request') && (response.statusText !== 'Unprocessable Entity')
           ? Promise.reject(response.statusText)
           : Promise.reject(response.json());
