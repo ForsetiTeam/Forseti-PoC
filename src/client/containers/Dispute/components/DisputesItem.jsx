7f2f5dd222299c7477a42a4f082a040ea4401ac4
zeleniy <zeleniy00@yandex.ru>
2018-06-08 16:31:12 +0700

    design - DisputeItem large content fixes

--- src/client/containers/Dispute/components/DisputesItem.jsx
+++ src/client/containers/Dispute/components/DisputesItem.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import truncate from 'html-truncate';
+
 class DisputesItem extends Component {
   static propTypes = {
     dispute: PropTypes.shape()
@@ -9,24 +11,21 @@ class DisputesItem extends Component {
 
   render() {
     const dispute = this.props.dispute;
-    console.log('DISPUTE', dispute);
 
     return (
       <div className='Card__content text-center'>
-        <h1>{dispute.title}</h1>
-        <div className='frsMuted'>
-          <p>{dispute.description}</p>
-          <dl className='row no-gutters text-left'>
-            <dt className='col-4 text-right'>Contract:</dt>
-            <dd className='col-8 text-truncate'>{dispute.ethAddress}</dd>
-            <dt className='col-4 text-right'>Respondent:</dt>
-            <dd className='col-8 text-truncate'>{dispute.authorAddress}</dd>
-            <dt className='col-4 text-right'>Applicant:</dt>
-            <dd className='col-8 text-truncate'>{dispute.authorAddress}</dd>
-            <dt className='col-4 text-right'>Community:</dt>
-            <dd className='col-8'>{dispute.communityName}</dd>
-          </dl>
-        </div>
+        <h1 className='text-truncate'>{dispute.title}</h1>
+        <p className='frsMuted flex-grow-1'>{truncate(dispute.description, 150)}</p>
+        <dl className='row no-gutters frsMuted text-left'>
+          <dt className='col-4 text-right'>Contract:</dt>
+          <dd className='col-8 text-truncate'>{dispute.ethAddress}</dd>
+          <dt className='col-4 text-right'>Respondent:</dt>
+          <dd className='col-8 text-truncate'>{dispute.authorAddress}</dd>
+          <dt className='col-4 text-right'>Applicant:</dt>
+          <dd className='col-8 text-truncate'>{dispute.authorAddress}</dd>
+          <dt className='col-4 text-right'>Community:</dt>
+          <dd className='col-8'>{dispute.communityName}</dd>
+        </dl>
         <div className='Card__bottom'>
           <Link to={`/dispute/${dispute.id}`} className='btn btn-primary'>Learn more</Link>
         </div>
