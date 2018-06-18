486ff4e86bb42ac2f908b89d1081c535b915e3dd
zeleniy <zeleniy00@yandex.ru>
2018-06-18 13:13:54 +0700

    fix select arbiters on server

--- src/server/ethereum/pool/getPoolReputationById.js
+++ src/server/ethereum/pool/getPoolReputationById.js
@@ -5,7 +5,7 @@ export default function getPoolReputationByAddress(poolAddress, id) {
     const pool = contractUtils.getSmartContract('Pool', poolAddress);
 
     contractUtils.runSigned(pool, 'membersById', [ id ], arbiter => {
-      resolve({ id, address: arbiter[1], reputation: arbiter[0] });
+      resolve({ id, address: arbiter[1], reputation: +arbiter[0] });
     }, reject);
   });
 }
